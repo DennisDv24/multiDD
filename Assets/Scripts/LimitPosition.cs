66ca95634a39b935b4e2cc106c3d7b9200ad3b34
Dennis <jokarboxing@gmail.com>
2019-12-24 17:11:04 +0100

    sadly fixed

--- Assets/Scripts/LimitPosition.cs
+++ Assets/Scripts/LimitPosition.cs
@@ -10,26 +10,19 @@ public bool yneg;
     int xoperator;
     int yoperator;
 
-    void Start()
+    void FixedUpdate()
     {
-            print("in:"+transform.position);
-
                 xoperator = xneg?-1:1;
                 yoperator = yneg?-1:1;
 
-            float posx = ((Mathf.Sqrt(2)/4)+(0.1f*Mathf.Sin(Mathf.PI/4)));
-            float posy = ((Mathf.Sqrt(2)/4)+(0.1f*Mathf.Sin(Mathf.PI/4)));
+            float posx = xoperator*((Mathf.Sqrt(2)/4)+(0.1f*Mathf.Sin(Mathf.PI/4)));
+            float posy = yoperator*((Mathf.Sqrt(2)/4)+(0.1f*Mathf.Sin(Mathf.PI/4)));
             float posz = -6.5f;
 
         Vector3 pos = new Vector3(posx, posy, posz); 
 
     transform.position = pos;
-
-        print("f:"+transform.position);
-
-            //state.rotation = Quaternion.Euler(-180,0,-45);
             
-            //transform.rotation = Quaternion.Euler(180,0,180-45);
     }
 
 }
